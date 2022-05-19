cf99695eda46832730a73801d3ffc774f31378c9
Rafath Bin Zafar Auvee <61088535+Rafath-Bin-Zafar-Auvee@users.noreply.github.com>
2022-05-19 18:14:18 +0600

    service api working

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
@@ -6,6 +7,30 @@ const port = process.env.PORT || 5000;
 
 app.use(cors())
 app.use(express.json())
+
+const uri = `mongodb+srv://${process.env.user}:${process.env.password}@cluster0.afkxs.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+  try {
+    await client.connect();
+    const data = client.db("doctors_portal").collection("service");
+
+    app.get("/service", async(req,res) =>{
+      const query = {};
+      const cursor = data.find(query)
+      const services = await cursor.toArray()
+      res.send(services)
+    })
+
+  } finally {
+    
+  }
+}
+
+
+run().catch(console.dir);
 
 app.get('/',(req,res)=>{
   res.send("Server is running")
