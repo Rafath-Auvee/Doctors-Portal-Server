e1927ed40760eaf4c3188f55367fe565188e3884
Rafath Bin Zafar Auvee <61088535+Rafath-Bin-Zafar-Auvee@users.noreply.github.com>
2022-05-21 15:44:17 +0600

    user creation process put method

--- index.js
+++ index.js
@@ -1,41 +1,107 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config();
-const app = express()
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.user}:${process.env.password}@cluster0.afkxs.mongodb.net/?retryWrites=true&w=majority`;
 
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
 
 async function run() {
   try {
     await client.connect();
     const data = client.db("doctors_portal").collection("service");
+    const appointment = client.db("doctors_portal").collection("bookings");
+    const userData = client.db("doctors_portal").collection("user");
 
-    app.get("/service", async(req,res) =>{
+    app.put("/user/:email", async (req, res) => {
+      const user = req.body;
+      const email = req.params.email;
+      const filter = { email: email };
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: user,
+      };
+      const result = await userData.updateOne(filter, updateDoc, options);
+      res.send(result);
+    });
+
+    app.get("/service", async (req, res) => {
       const query = {};
-      const cursor = data.find(query)
-      const services = await cursor.toArray()
-      res.send(services)
-    })
+      const cursor = data.find(query);
+      const services = await cursor.toArray();
+      res.send(services);
+    });
 
+    app.post("/booking", async (req, res) => {
+      const booking = req.body;
+      const query = {
+        treatment: booking.treatment,
+        date: booking.date,
+        patient: booking.patient,
+      };
+      const exists = await appointment.findOne(query);
+      if (exists) {
+        return res.send({ success: false, booking: exists });
+      }
+      const result = await appointment.insertOne(booking);
+      return res.send({ success: true, result });
+    });
+
+    app.get("/available", async (req, res) => {
+      const date = req.query.date;
+
+      // step 1:  get all services
+      const services = await data.find().toArray();
+
+      // step 2: get the booking of that day. output: [{}, {}, {}, {}, {}, {}]
+      const query = { date: date };
+      const bookings = await appointment.find(query).toArray();
+
+      // step 3: for each service
+      services.forEach((service) => {
+        // step 4: find bookings for that service. output: [{}, {}, {}, {}]
+        const serviceBookings = bookings.filter(
+          (book) => book.treatment === service.name
+        );
+        // step 5: select slots for the service Bookings: ['', '', '', '']
+        const bookedSlots = serviceBookings.map((book) => book.slot);
+        // step 6: select those slots that are not in bookedSlots
+        const available = service.slots.filter(
+          (slot) => !bookedSlots.includes(slot)
+        );
+        //step 7: set available to slots to make it easier
+        service.slots = available;
+      });
+
+      res.send(services);
+    });
+
+    app.get("/booking", async (req, res) => {
+      const patient = req.query.patient;
+      const query = { patient: patient };
+      const bookings = await appointment.find(query).toArray();
+      res.send(bookings);
+    });
   } finally {
-    
   }
 }
 
-
 run().catch(console.dir);
 
-app.get('/',(req,res)=>{
-  res.send("Server is running")
-})
+app.get("/", (req, res) => {
+  res.send("Server is running");
+});
 
-app.listen(port, ()=>{
-  console.log(`Server is working ${port}`)
-})
+app.listen(port, () => {
+  console.log(`Server is working ${port}`);
+});
