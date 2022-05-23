3c3ff6dada7f13893bab0777ef195bcdc7fc4c74
Rafath Bin Zafar Auvee <61088535+Rafath-Bin-Zafar-Auvee@users.noreply.github.com>
2022-05-23 14:31:41 +0600

    client screct

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const express = require("express");
 const jwt = require("jsonwebtoken");
 const cors = require("cors");
+const stripe = require('stripe')(process.env.STRIPE_KEY);
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -9,6 +10,11 @@ var nodemailer = require("nodemailer");
 var sgTransport = require("nodemailer-sendgrid-transport");
 app.use(cors());
 app.use(express.json());
+
+
+// sk_test_51BoX61IXEsb8wMTRMSwhAWJU4ZwO3VquJsFoxiSA4GYEIVEZv9Qz91sKRHYUKb4w5h1jHGb0B0zEKQBECGsmlyGc00kZHJ5q1R
+
+
 
 const uri = `mongodb+srv://${process.env.user}:${process.env.password}@cluster0.afkxs.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -89,6 +95,18 @@ async function run() {
         res.status(403).send({ message: "forbidden" });
       }
     };
+
+    app.post('/create-payment-intent', verifyJWT, async(req, res) =>{
+      const service = req.body;
+      const price = service.price;
+      const amount = price*100;
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount : amount,
+        currency: 'usd',
+        payment_method_types:['card']
+      });
+      res.send({clientSecret: paymentIntent.client_secret})
+    });
 
     app.get("/service", async (req, res) => {
       const query = {};
